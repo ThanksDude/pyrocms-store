093de92f35a3eff98cc3bc7770ae033dcee1a6bf
Eric Yang <tomdy.net@gmail.com>
2012-01-18 13:31:52 +0800

    Update widgets/store_categories/store_categories.php

--- widgets/store_categories/store_categories.php
+++ widgets/store_categories/store_categories.php
@@ -12,12 +12,14 @@ class Widget_Store_categories extends Widgets {
 	public $title = array(
 		'en' => 'Store Categories',
 		'nl' => 'Winkel Categorieen',
-		'de' => 'Store Categories'
+		'de' => 'Store Categories',
+		'zh' => '商店產品分類'
 	);
 	public $description	= array(
 		'en' => 'Display the Store Categories',
 		'nl' => 'Toon de Winkel Categorieen',
-		'de' => 'Zeigen Sie die Store Categories'
+		'de' => 'Zeigen Sie die Store Categories',
+		'zh' => '顯示商店產品分類'
 	);
 	public $author		= 'Jaap Jolman - Kevin Meier - Rudolph Arthur Hernandez - Gary Hussey';
 	public $website		= 'http://www.odin-ict.nl/';
