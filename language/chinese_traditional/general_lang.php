607cd44eb5053cef647a66acdc67ddc1ec820cde
Eric Yang <tomdy.net@gmail.com>
2012-01-19 10:55:03 +0800

    Update language/chinese_traditional/general_lang.php

--- language/chinese_traditional/general_lang.php
+++ language/chinese_traditional/general_lang.php
@@ -3,7 +3,7 @@
 /**
  * This is a store module for PyroCMS
  *
- * @author 		pyrocms-store Team - Jaap Jolman - Kevin Meier - Rudolph Arthur Hernandez - Gary Hussey
+ * @author 		pyrocms-store Team - Jaap Jolman - Kevin Meier - Rudolph Arthur Hernandez - Gary Hussey - Eric Yang
  * @website		http://www.odin-ict.nl/
  * @package 	pyrocms-store
  * @subpackage 	Store Module
@@ -14,8 +14,11 @@
 // MENU ------------------------------------------------------------------------------------------
 
 $lang['store_menu_store']							= '商店';
-$lang['store_menu_products']						= '產品';
-$lang['store_menu_categories']						= '分類';
+$lang['store_menu_products']					= '產品';
+$lang['store_menu_categories']				= '分類';
+$lang['store_menu_tags']  						= '標籤';
+$lang['store_menu_attributes']				= '屬性';
+
 
 // SHORTCUTS -------------------------------------------------------------------------------------
 
@@ -24,24 +27,30 @@ $lang['store_shortcut_store_statistics']			= '統計';
 $lang['store_shortcut_store_settings']				= '設置';
 
 $lang['store_shortcut_categories_list']				= '分類列表';
-$lang['store_shortcut_category_add']				= '添加分類';
+$lang['store_shortcut_category_add']			  	= '添加分類';
 
-$lang['store_shortcut_products_list']				= '清單產品';
-$lang['store_shortcut_product_add']					= '添加產品';
+$lang['store_shortcut_products_list']				  = '清單產品';
+$lang['store_shortcut_product_add']					  = '添加產品';
+
+$lang['store_shortcut_tags_list']  		  	  	= '標籤列表';
+$lang['store_shortcut_tag_add']					    	= '新增標籤';
+
+$lang['store_shortcut_attributes_list']				= '屬性列表';
+$lang['store_shortcut_attribute_add']				  = '新增屬性';
 
 // TITLES ----------------------------------------------------------------------------------------
 
-$lang['store_title_store_dashboard']         		= '商店列表';
-$lang['store_title_store_statistics']         		= '統計';
-$lang['store_title_store_settings']         		= '設置';
+$lang['store_title_store_dashboard']         	= '商店列表';
+$lang['store_title_store_statistics']         = '統計';
+$lang['store_title_store_settings']         	= '設置';
 
 $lang['store_title_category_list']       			= '分類列表';
 $lang['store_title_category_add']        	 		= '添加分類';
-$lang['store_title_category_edit'] 					= '編輯類別';
+$lang['store_title_category_edit'] 					  = '編輯類別';
 
 $lang['store_title_product_list']        			= '清單產品';
-$lang['store_title_product_add']					= '添加產品';
-$lang['store_title_product_edit']					= '編輯產品';
+$lang['store_title_product_add']					    = '添加產品';
+$lang['store_title_product_edit']					    = '編輯產品';
 
 /* ========================================= FORM TYPES ======================================== */
 
