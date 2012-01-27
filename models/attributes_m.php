ce4b007b908b5a25eb86ff7f851c3854942ef458
Marijan Greguric <marijan.greguric@gmail.com>
2012-01-27 16:37:04 +0100

    Update models/attributes_m.php

--- models/attributes_m.php
+++ models/attributes_m.php
@@ -19,4 +19,15 @@ class Attributes_m extends MY_Model
 		$this->load->library('store_settings');
 		$this->_store = $this->store_settings->item('store_id');
 	}
+	/**  
+	 * Add Attribute
+     	* @param array of input
+     	* @return true or false
+     	*/	
+	public function add_attribute($input)
+	{
+	   $insert_data['name'] =  $input['name'];
+	   $insert_data['html'] =  $input['html'];
+	   return $this->db->insert($this->_table, $insert_data) ? $this->db->insert_id() : FALSE;
+	}
 }
