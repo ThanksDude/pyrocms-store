328077549303d6b347c329c0a21e6d52329af554
Christopher Manouvrier <cmanou@mac.com>
2012-02-19 21:45:04 +1100

    Update Backend Categories Index
    Added Parent Categories name instead of id

--- language/english/categories_lang.php
+++ language/english/categories_lang.php
@@ -38,6 +38,8 @@ $lang['store:category:label:parent']					= 'Parent';
 $lang['store:category:label:thumbnail']					= 'Thumbnail';
 $lang['store:category:label:images_id']					= 'Images';
 $lang['store:category:label:actions']					= 'Actions';
+$lang['store:category:label:no_parent']					= 'No Parent';
+
 
 // MESSAGES --------------------------------------------------------------------------------------
 
